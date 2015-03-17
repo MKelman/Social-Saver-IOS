167d68d7bc0380804741e94b9a0a42a937eb4071
Mitchell Kelman <MitchellKelman@gmail.com>
2015-03-17 16:34:46 -0400

    more changes

--- SocialSaver/MainViewController.h
+++ SocialSaver/MainViewController.h
@@ -11,5 +11,18 @@
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *sideButton;
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *dealType;
+@property (weak, nonatomic) IBOutlet UITextField *itemName;
+@property (weak, nonatomic) IBOutlet UITextField *maxPrice;
+@property (weak, nonatomic) IBOutlet UILabel *saleEndLabel;
+@property (weak, nonatomic) IBOutlet UITextField *saleEnd;
+@property (weak, nonatomic) IBOutlet UILabel *foundLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *found;
+@property (weak, nonatomic) IBOutlet UITextField *foundLocation;
+- (IBAction)submitButton:(id)sender;
+- (IBAction)DealTypeAction:(id)sender;
+
+
+
 
 @end
--- SocialSaver/MainViewController.m
+++ SocialSaver/MainViewController.m
@@ -19,7 +19,14 @@
 @end
 
 @implementation MainViewController
-NSString *usertext;
+@synthesize dealType,itemName,maxPrice,saleEnd,saleEndLabel,found,foundLabel,foundLocation;
+
+static const CGFloat KEYBOARD_ANIMATION_DURATION = 0.3;
+static const CGFloat MINIMUM_SCROLL_FRACTION = 0.2;
+static const CGFloat MAXIMUM_SCROLL_FRACTION = 0.8;
+static const CGFloat PORTRAIT_KEYBOARD_HEIGHT = 216;
+static const CGFloat LANDSCAPE_KEYBOARD_HEIGHT = 162;
+CGFloat animatedDistance;
 
 
 - (void)viewDidLoad
@@ -35,9 +42,14 @@ NSString *usertext;
     
     // Set the gesture to slide the sidebarviewcontroller
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
-    // Change button color
-    //_optionsButton.tintColor = [UIColor colorWithWhite:0.1f alpha:0.9f];
-    //reset the current installation just in case! changed in another installation on another phone!
+    
+    self.itemName.delegate = self;
+    self.maxPrice.delegate = self;
+    self.saleEnd.delegate = self;
+    self.foundLocation.delegate = self;
+
+
+
 
 }
 
@@ -46,6 +58,134 @@ NSString *usertext;
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event { //allows user to click out of edittext field
+    //[self.view endEditing:YES];
+    [itemName resignFirstResponder];
+    [maxPrice resignFirstResponder];
+    [saleEnd resignFirstResponder];
+    [foundLocation resignFirstResponder];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [itemName resignFirstResponder];
+    [maxPrice resignFirstResponder];
+    [saleEnd resignFirstResponder];
+    [foundLocation resignFirstResponder];
+    return YES;
+}
+
+
+
+- (IBAction)submitButton:(id)sender {
+    NSString *choice = [dealType titleForSegmentAtIndex : dealType.selectedSegmentIndex];
+    if ([choice isEqualToString:@"Found Deal"]) {
+        if ( [itemName text].length <= 0
+            || [maxPrice text].length <= 0
+            || [saleEnd text].length <= 0
+            || [foundLocation text].length <= 0) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Entry" message:@"Sorry, not all the fields have been filled in." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [alert show];
+        } else {
+            PFObject *foundDeal = [PFObject objectWithClassName:@"FoundDeals"];
+            //foundDeal[@"State"] = TOMIstate;
+
+            
+        }
+    } else {
+        if ( [itemName text].length <= 0 || [maxPrice text].length <= 0) {
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Invalid Entry" message:@"Sorry, not all the fields have been filled in." delegate:self cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [alert show];
+        } else {
+            PFObject *wantedDeal = [PFObject objectWithClassName:@"SeekingDeals"];
+            wantedDeal[@"maxPrice", (NSNumber*)[maxPrice text]];
+            //wantedDeal[@"userLocation", point];
+            wantedDeal[@"userEmail"] = [[PFUser currentUser]objectForKey:@"email"];
+            wantedDeal[@"item", [itemName text]];
+            [wantedDeal saveInBackground];
+
+            
+        }
+    }
+    
+}
+
+- (IBAction)DealTypeAction:(id)sender {
+    NSString *choice = [dealType titleForSegmentAtIndex : dealType.selectedSegmentIndex];
+    if ([choice isEqualToString:@"Found Deal"]) {
+        [saleEndLabel setHidden:NO];
+        [saleEnd setHidden:NO];
+        [foundLabel setHidden:NO];
+        [found setHidden:NO];
+        [foundLocation setHidden:NO];
+    } else {
+        [saleEndLabel setHidden:YES];
+        [saleEnd setHidden:YES];
+        [foundLabel setHidden:YES];
+        [found setHidden:YES];
+        [foundLocation setHidden:YES];
+    }
+}
+
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    CGRect textFieldRect =
+    [self.view.window convertRect:textField.bounds fromView:textField];
+    CGRect viewRect =
+    [self.view.window convertRect:self.view.bounds fromView:self.view];
+    
+    CGFloat midline = textFieldRect.origin.y + 0.5 * textFieldRect.size.height;
+    CGFloat numerator = midline - viewRect.origin.y
+    - MINIMUM_SCROLL_FRACTION * viewRect.size.height;
+    CGFloat denominator =
+    (MAXIMUM_SCROLL_FRACTION - MINIMUM_SCROLL_FRACTION)
+    * viewRect.size.height;
+    CGFloat heightFraction = numerator / denominator;
+    
+    if (heightFraction < 0.0){
+        heightFraction = 0.0;
+    }
+    else if (heightFraction > 1.0){
+        heightFraction = 1.0;
+    }
+    UIInterfaceOrientation orientation =
+    [[UIApplication sharedApplication] statusBarOrientation];
+    if (orientation == UIInterfaceOrientationPortrait ||
+        orientation == UIInterfaceOrientationPortraitUpsideDown)
+    {
+        animatedDistance = floor(PORTRAIT_KEYBOARD_HEIGHT * heightFraction);
+    }
+    else{
+        animatedDistance = floor(LANDSCAPE_KEYBOARD_HEIGHT * heightFraction);
+    }
+    
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y -= animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    [self.view setFrame:viewFrame];
+    [UIView commitAnimations];
+    
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    CGRect viewFrame = self.view.frame;
+    viewFrame.origin.y += animatedDistance;
+    
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:KEYBOARD_ANIMATION_DURATION];
+    
+    [self.view setFrame:viewFrame];
+    
+    [UIView commitAnimations];
+}
+
 
 
 
